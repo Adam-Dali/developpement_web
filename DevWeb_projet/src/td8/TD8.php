bf8bd18dd5116520653ff0b9bea4d2d1f19e0606
Adam Dali <adam.dali7@etu.univ-lorraine.fr>
2025-11-01 19:46:36 +0100

    Ajout tp8

---
+++ DevWeb_projet/src/td8/TD8.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Si la désactivation de la temporisation de sortie ne fonctionne pas avec le .htaccess 
+ * fourni, il est possible de forcer l'envoi les données du tampon de sortie et d'éteindre 
+ * la temporisation de sortie avec la fonction ob_end_flush() au début du script.
+ */
+
+ob_start();
+http_response_code(201);
+echo '<pre>'; 
+var_dump($_SERVER);
+header("newHeaderTD8: chaipakoimaitre");
